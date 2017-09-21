5940de96fe52a8229d02bd8f6b615f2a8b532171
Akshay Khot <akshay.7277@gmail.com>
2017-09-21 09:50:18 -0700

    add support for extra fields and flow_css

--- modules/ae/src/Plugin/Block/HelloBlock.php
+++ modules/ae/src/Plugin/Block/HelloBlock.php
@@ -31,10 +31,35 @@ class HelloBlock extends BlockBase {
             $options[$social] = $social;
         }
 
+        $fields = [
+            'email' => "Email",
+            'dob' => "Date Of Birth",
+            'add1' => "Address Line 1",
+            'add2' => "Address Line 2",
+            'city' => "City",
+            'mobile' => "Mobile Number",
+            'firstname' => "First Name",
+            'lastname' => "Last Name",
+            'zip' => "Zipcode",
+            'country' => "Country"
+        ];
+
         $form['socials'] = array(
             '#type' => 'checkboxes',
             '#options' => $options,
             '#title' => $this->t('Select the social logins that you want to add'),
+        );
+
+        $form['flow_css'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Enter the URL for the css')
+        ];
+
+        $form['extra_fields'] = array(
+            '#type' => 'checkboxes',
+            '#options' => $fields,
+            '#title' => $this->t('Required Fields'),
+            '#description' => $this->t('Define fields you require to be collected.')
         );
 
         $form['auth_window'] = array(
@@ -51,30 +76,48 @@ class HelloBlock extends BlockBase {
             '#options' => array(0 => $this->t('Yes'), 1 => $this->t('No')),
         );
 
+        $form['sso'] = array(
+            '#type' => 'radios',
+            '#title' => $this->t('Do you want to enable single sign on?'),
+            '#default_value' => 1,
+            '#options' => array(0 => $this->t('Yes'), 1 => $this->t('No')),
+        );
+
         return $form;
       }
 
     public function blockSubmit($form, FormStateInterface $form_state) {
 
         $socials = $this->getSelectedSocials($form_state);
+        $fields = $this->getSelectedFields($form_state);
         $auth_window = $form_state->getValue('auth_window'); 
         $want_email = $form_state->getValue('email_signup');
+        $sso = $form_state->getValue('sso');
+        $flow_css = $form_state->getValue('flow_css');
+
         $this->state->set('socials', $socials);
+        $this->state->set('fields', $fields);
         $this->state->set('email_signup', $want_email);
         $this->state->set('auth_window', $auth_window);
+        $this->state->set('sso', $sso);
+        $this->state->set('flow_css', $flow_css);
+        
     }
 
     public function build() {
         //return \Drupal::formBuilder()->getForm('Drupal\ae\Form\SignupForm');
 
-        $socials = $this->state->get('socials');
-        $urls = explode("|", $socials);
+        $urls = explode("|", $this->state->get('socials'));
+        $fields = explode("|", $this->state->get('fields'));
 
         return [
             '#theme' => 'signup',
             '#socials' => $urls,
+            '#fields' => $fields,
             '#want_email' => $this->state->get('email_signup'),
             '#auth_window' => $this->state->get('auth_window'),
+            '#sso' => $this->state->get('sso'),
+            '#flow_css' => $this->state->get('flow_css'),
             '#attached' => array(
                 'library' => array(
                   'ae/script',
@@ -98,6 +141,13 @@ class HelloBlock extends BlockBase {
         $socials = $form_state->getValue('socials');
         $socials = array_diff($socials, [0]);
         $value = implode("|", $socials);
+        return $value;
+    }
+
+    public function getSelectedFields(FormStateInterface $form_state) {
+        $fields = $form_state->getValue('extra_fields');
+        $fields = array_diff($fields, [0]);
+        $value = implode("|", $fields);
         return $value;
     }
 }
