6b01caea26bc5041f6f8edd3c773144ddbd8f745
DESKTOP-7AB715A\aksha <akshay@appreciationengine.com>
2017-09-28 15:32:52 -0700

    extract fields into settings

--- modules/ae/src/Form/AeSettingsForm.php
+++ modules/ae/src/Form/AeSettingsForm.php
@@ -57,6 +57,13 @@ class AeSettingsForm extends ConfigFormBase {
             '#options' => array(0 => $this->t('Yes'), 1 => $this->t('No')),
         );
 
+        $form['extra_fields'] = array(
+            '#type' => 'checkboxes',
+            '#options' => $this->getFields(),
+            '#title' => $this->t('Required Fields'),
+            '#description' => $this->t('Define fields you require to be collected.')
+        );
+
 
         return parent::buildForm($form, $form_state);
     }
@@ -64,12 +71,12 @@ class AeSettingsForm extends ConfigFormBase {
     public function submitForm(array &$form, FormStateInterface $form_state) {
         parent::submitForm($form, $form_state);
 
+        $fields = $this->getSelectedFields($form_state);
         $auth_window = $form_state->getValue('auth_window');
         $want_email = $form_state->getValue('email_signup');
         $sso = $form_state->getValue('sso');
         $flow_css = $form_state->getValue('flow_css');
         $new_user = $form_state->getValue('new_user');
-
 
         $state = \Drupal::state();
         $state->set('email_signup', $want_email);
@@ -77,8 +84,37 @@ class AeSettingsForm extends ConfigFormBase {
         $state->set('sso', $sso);
         $state->set('flow_css', $flow_css);
         $state->set('new_user', $new_user);
+        $state->set('fields', $fields);
 
         drupal_set_message($flow_css);
+    }
+
+    private function getFields() {
+        return [
+            'email' => "Email",
+            'password' => "Password",
+            'birthdate' => "Date Of Birth",
+            'address' => "Address",
+            'addressline2' => "Address Line 2",
+            'city' => "City",
+            'state' => "State",
+            'homephone' => "Home Phone",
+            'mobilephone' => "Mobile Phone",
+            'firstname' => "First Name",
+            'username' => "Username",
+            'website' => "Website",
+            'bio' => "Bio",
+            'gender' => "Gender",
+            'surname' => "Last Name",
+            'postcode' => "Zipcode",
+            'country' => "Country"
+        ];
+    }
+
+    private function getSelectedFields(FormStateInterface $form_state) {
+        $fields = $form_state->getValue('extra_fields');
+        $fields = array_diff($fields, [0]);
+        return $fields;
     }
 
 
--- modules/ae/src/Plugin/Block/HelloBlock.php
+++ modules/ae/src/Plugin/Block/HelloBlock.php
@@ -31,38 +31,10 @@ class HelloBlock extends BlockBase {
             $options[$social] = $social;
         }
 
-        $fields = [
-            'email' => "Email",
-            'password' => "Password",
-            'birthdate' => "Date Of Birth",
-            'address' => "Address",
-            'addressline2' => "Address Line 2",
-            'city' => "City",
-            'state' => "State",
-            'homephone' => "Home Phone",
-            'mobilephone' => "Mobile Phone",
-            'firstname' => "First Name",
-            'username' => "Username",
-            'website' => "Website",
-            'bio' => "Bio",
-            'gender' => "Gender",
-            'surname' => "Last Name",
-            'postcode' => "Zipcode",
-            'country' => "Country"
-        ];
-
         $form['socials'] = array(
             '#type' => 'checkboxes',
             '#options' => $options,
             '#title' => $this->t('Select the social logins that you want to add'),
-        );
-
-
-        $form['extra_fields'] = array(
-            '#type' => 'checkboxes',
-            '#options' => $fields,
-            '#title' => $this->t('Required Fields'),
-            '#description' => $this->t('Define fields you require to be collected.')
         );
 
         return $form;
@@ -71,16 +43,14 @@ class HelloBlock extends BlockBase {
     public function blockSubmit($form, FormStateInterface $form_state) {
 
         $socials = $this->getSelectedSocials($form_state);
-        $fields = $this->getSelectedFields($form_state);
+
 
         $this->state->set('socials', $socials);
-        $this->state->set('fields', $fields);
+
         
     }
 
     public function build() {
-        //return \Drupal::formBuilder()->getForm('Drupal\ae\Form\SignupForm');
-
 
         return [
             '#theme' => 'signup',
@@ -92,11 +62,11 @@ class HelloBlock extends BlockBase {
             '#new_user' => $this->state->get('new_user'),
             '#flow_css' => $this->state->get('flow_css'),
             '#extra_info' => $this->state->get('extra_info'),
-            '#attached' => array(
-                'library' => array(
+            '#attached' => [
+                'library' => [
                   'ae/script',
-                ),
-              ),
+                ],
+              ],
           ];
     }
 
@@ -118,11 +88,5 @@ class HelloBlock extends BlockBase {
         return $socials;
     }
 
-    public function getSelectedFields(FormStateInterface $form_state) {
-        $fields = $form_state->getValue('extra_fields');
-        $fields = array_diff($fields, [0]);
-        //$value = implode("|", $fields);
-        return $fields;
-    }
 }
 ?>
