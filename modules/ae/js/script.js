dc8d3871ce1eb507a24f3c4e7a500f6ecffdd92a
DESKTOP-7AB715A\aksha <akshay@appreciationengine.com>
2017-10-03 13:36:53 -0700

    Initial commit.

--- modules/ae/js/script.js
+++ modules/ae/js/script.js
@@ -1,64 +1,53 @@
 var globalAEJS;
 var state = 'none';
 
+
 function flowHandler(event) {
     console.log("FLOW HANDLER");
     console.log(event);
 
-	if (event.step === 'required-fields') {
-		$('#signup').hide();
+    if (event.step === 'required-fields') {
+        $('#signup').hide();
         $('#additional-data').show();
         $("#loggedin").show();
-	}     
+    }
 }
 function windowHandler(event) {
-	console.log("WINDOW HANDLER");
+	  console.log("WINDOW HANDLER");
     console.log(event);
 }
 function loginHandler(user,type,sso) {
-	console.log("LOGIN HANDLER:"+type);
+	  console.log("LOGIN HANDLER:"+type);
     console.log(user);
-    //console.log(sso);
     alert("SIGNED UP. THANKS.");
     $('#signup').hide();
-     $("#loggedin").show(); //allow logout
+    $("#loggedin").show(); //allow logout
 
-     $.ajax(
-         {
-             url: '/ae/ajax/' + user.data.AccessToken,
-             method: 'GET',
-             success: function(data) {
-                 console.log("User saved successfully. User Id:  " + data);
-             }
-         }
-     );
-     
-    //$("#additional-data-form").off('submit'); //remove AE handlers
-    //$("#additional-data-form").attr('action', "sony_sample_handler.php?MemberID="+user.data.ID); //set local handler url
-	//$("#additional-data-form").submit(); //data already saved to AE - submit form to a local handler
+    $.ajax({
+        url: '/ae/ajax/' + user.data.ID,
+        method: 'GET',
+        success: function(data) {
+          console.log("User saved successfully. User Id:  " + data);
+        }
+    });
 }
 function userHandler(user,state) {
-	console.log("USER HANDLER");
+	  console.log("USER HANDLER");
     console.log(user);
     console.log(state);
     
     $("#loggedin").show(); //allow logout
 
-    // if (state == 'update') {
-    // 	$("#additional-data-form").off('submit'); //remove AE handlers
-    // 	$("#additional-data-form").attr('action', "sony_sample_handler.php"); //set local handler url
-	// 	$("#additional-data-form").submit(); //data already saved to AE - submit form to a local handler
-	// }   
 }
 function logoutHandler(user) {
-	console.log("LOGOUT HANDLER");
+	  console.log("LOGOUT HANDLER");
     console.log(user);
     alert("LOGGED OUT. THANKS.")
     $("#loggedin").hide();
     $('#signup').show();
     $('#additional-data').hide();
     $( "a.ae-btn" ).each(function( index ) {
-  		$( this ).removeClass('active');
-	});
+      $( this ).removeClass('active');
+	  });
 }
 
--- modules/ae/src/Controller/LoginController.php
+++ modules/ae/src/Controller/LoginController.php
@@ -9,30 +9,39 @@ use Drupal\user\Entity\User;
 class LoginController extends ControllerBase {
 
     public function createuser($id) {
-        
-        // $state = \Drupal::state();
+      // fetch the AE user
+      // Store that user in the database
+      // Create a nwe Drupal user
+      // Map 1:1 AE user to drupal user
 
-        // $api_key = $state->get('api_key');
-        // $url = 'https://akshay.dev.appreciationengine.com/v1.1/auth?apiKey=' . $api_key . '&accesstoken='. $id;
+          $state = \Drupal::state();
 
-        // // $u = "https://akshay.dev.appreciationengine.com/v1.1/auth?apiKey=9ee609a0370231ac93149413e00a2ca0&accesstoken=d732a4174b10f5b255c7cb2f953c559c_1506459361";
-        
-        // $client = \Drupal::httpClient();
-        // $request = $client->get($url);   
-        // $response = (string) $request->getBody();
+          $api_key = $state->get('api_key');
+          $url = "https://akshay.dev.appreciationengine.com/v1.1/member/" . $id . "?apiKey=" . $api_key;
 
-        // $ae_user = Json::decode($response);
-        
-        $user = User::create();
-        $user->setPassword('password');
-        $user->enforceIsNew();
-        $user->setEmail('akshay' . rand(0, 99) . '@appengine12.com');
-        $user->setUsername('user' . rand(0, 99));
-        $result = $user->save();
+          $client = \Drupal::httpClient();
+          $request = $client->get($url);
+          $response = (string) $request->getBody();
 
-        echo $user->uuid();
+          $ae_user = Json::decode($response);
 
-        exit(0);
+          $user = User::create();
+          $user->setPassword('password');
+          $user->enforceIsNew();
+          $user->setEmail($ae_user->Email);
+          $user->setUsername($ae_user->Username);
+          $result = $user->save();
+
+          db_insert('ae_user')->fields([
+            'aeid' => 1,
+            'firstname' => "Akshay",
+            'surname' => "Khot",
+            'username' => "akki03"
+          ])->execute();
+
+          echo $user->uuid();
+
+          exit(0);
     }
 
     function createNewUser() {
